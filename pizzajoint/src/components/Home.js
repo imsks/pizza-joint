938a8744e1bd74d90e5045b4f97afb1de5a13dd9
Sachin <sachinkshuklaoo7@gmail.com>
2020-12-17 23:17:34 +0530

    working on animation

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div
+      className="home container"
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{delay: 1.5}}
+    >
+      <motion.h2 animate={{ fontSize: 50, color: "green" }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button animate={{ scale: 1.5 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
